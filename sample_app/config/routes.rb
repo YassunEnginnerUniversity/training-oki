1ba11142053db1c48e92a0b941c45fea099b7b68
kei-1313 <candydog666@gmail.com>
2024-10-25 22:01:17 +0800

    add: 5.4ユーザー登録: 最初のステップ

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get "/help", to: "static_pages#help", as: "helf"
   get "/about", to: "static_pages#about"
   get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new", as: "signup"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
