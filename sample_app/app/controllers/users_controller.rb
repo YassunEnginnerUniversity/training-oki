71042d607de409ad89538692d658195529dcb4d3
kei-1313 <candydog666@gmail.com>
2024-10-26 15:16:36 +0800

    add: 7.3.3エラーメッセージ

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -3,7 +3,22 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う。
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
   def new
     @user = User.new
   end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+    end
 end
