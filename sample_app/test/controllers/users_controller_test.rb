9969fb6703ddcd0e442702827c508722bf8a16fa
kei-1313 <candydog666@gmail.com>
2024-10-27 18:31:26 +0800

    add: 10.4.3ユーザー削除のテスト

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -54,4 +54,23 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     
     assert_not @other_user.admin?
   end
+
+  test "should redirect destroy when not logged in" do
+    # ユーザが変化しない
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    # 303でリダイレクトされる
+    assert_response :see_other
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
 end
