2839a54ce84a4b035feb9a60f08084ed5eef8573
kei-1313 <candydog666@gmail.com>
2024-10-27 12:54:35 +0800

    add: 9.1.1記憶トークンと暗号化, 9.1.2ログイン状態の保持

--- sample_app/app/helpers/sessions_helper.rb
+++ sample_app/app/helpers/sessions_helper.rb
@@ -3,9 +3,22 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  # 永続的セッションのためにユーザーをデータベースに記憶する
+  def remember(user)
+    user.remember
+    cookies.permanent.encrypted[:user_id] = user.id # user_idを暗号化してcookieに永続的に保存する
+    cookies.permanent[:remember_token] = user.remember_token # remember_tokenをcookieに永続的に保存する
+  end
+
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.encrypted[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
 
