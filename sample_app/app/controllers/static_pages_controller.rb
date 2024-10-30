cd4d431a82d93230c5887995185fb88ebeab7c33
kei-1313 <candydog666@gmail.com>
2024-10-31 02:11:00 +0800

    add: 13.3.3フィードの原型 , 13.3.4マイクロポストを削除する

--- sample_app/app/controllers/static_pages_controller.rb
+++ sample_app/app/controllers/static_pages_controller.rb
@@ -1,6 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if logged_in? #ログインしていれば空のmicropostインスタンスを作成している
+    if logged_in?
+      @micropost  = current_user.microposts.build #ログインしていれば空のmicropostインスタンスを作成している
+      @feed_items = current_user.feed.paginate(page: params[:page])
+    end
   end
 
   def help
