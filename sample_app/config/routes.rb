419f3633575ed13963736cd3e8a0dfd68e77571c
kei-1313 <candydog666@gmail.com>
2024-11-02 02:44:50 +0800

    add: 14.2.2統計と［Follow］フォーム,14.2.3［Following］と［Followers］ページ,

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -9,11 +9,16 @@ Rails.application.routes.draw do
   post "/login",   to: "sessions#create"
   delete "/logout",   to: "sessions#destory"
 
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
   get '/microposts', to: 'static_pages#home'
+  resources :relationships,       only: [:create, :destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
