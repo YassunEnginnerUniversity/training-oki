3870ea4c3e3f16850fb9459ebf666c2fc696fc79
kei-1313 <candydog666@gmail.com>
2024-11-03 00:51:07 +0800

    remove: gitのキャッシュをクリアしgitignoreを適用させる

--- rails-tutorial/sample_app/test/helpers/application_helper_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class ApplicationHelperText < ActionView::TestCase
-  test "full title helper" do
-    assert_equal "Ruby on Rails Tutorial Sample App" , full_title
-    assert_equal "Contact | Ruby on Rails Tutorial Sample App" , full_title("Contact")
-    assert_equal "Sign up | Ruby on Rails Tutorial Sample App" , full_title("Sign up")
-  end
-end
