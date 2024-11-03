13172a9df350863eba9a13ba35570777eba1ce70
kei-1313 <candydog666@gmail.com>
2024-11-03 12:01:27 +0800

    add: Sessionコントローラーを追加

--- api-and-rspec-training/config/routes.rb
+++ api-and-rspec-training/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   namespace :api do
+    get "sessions/create"
     resources :users, only: [:show]
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
