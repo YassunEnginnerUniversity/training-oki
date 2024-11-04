4711ea7a8601f24fe031973691335b00e528d6ad
kei-1313 <candydog666@gmail.com>
2024-11-04 13:45:08 +0800

    add: Postsモデルとコントローラーを追加

--- api-and-rspec-training/db/schema.rb
+++ api-and-rspec-training/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_03_081203) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_04_051330) do
+  create_table "posts", force: :cascade do |t|
+    t.text "content", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username", null: false
     t.string "password_digest"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_03_081203) do
     t.datetime "updated_at", null: false
     t.index ["username"], name: "index_users_on_username", unique: true
   end
+
+  add_foreign_key "posts", "users"
 end
