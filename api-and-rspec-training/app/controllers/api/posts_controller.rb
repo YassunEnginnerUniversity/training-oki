e53d6bac9f69ed3c96376638fe21713c40629ceb
kei-1313 <candydog666@gmail.com>
2024-11-04 19:52:03 +0800

    add: GET /api/posts/{id}の作成とテストの作成

--- api-and-rspec-training/app/controllers/api/posts_controller.rb
+++ api-and-rspec-training/app/controllers/api/posts_controller.rb
@@ -7,6 +7,11 @@ class Api::PostsController < ApplicationController
   end
 
   def show
+    post = Post.find(params[:id])
+    render json: post, status: :ok
 
+    # 例外処理（findは、値が取得できなかった場合にActiveRecord::RecordNotFoundを返す）
+    rescue ActiveRecord::RecordNotFound
+      render json: { error: "該当する投稿が見つかりませんでした。" }, status: :not_found
   end
 end
