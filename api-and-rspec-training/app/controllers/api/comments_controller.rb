27a62660d11ae5d42b758440d6912f070e4b0795
kei-1313 <candydog666@gmail.com>
2024-11-07 00:45:44 +0800

    add: 該当の投稿を取得し、コメントをDBに保存する

--- api-and-rspec-training/app/controllers/api/comments_controller.rb
+++ api-and-rspec-training/app/controllers/api/comments_controller.rb
@@ -1,5 +1,23 @@
 class Api::CommentsController < ApplicationController
   def create
-    
+    if comment_params[:content].blank?
+      return render json: { error: "コメントが空です。" }, status: :unprocessable_entity
+    end
+
+    post = Post.find(params[:post_id])
+    comment = post.comments.build(comment_params)
+    comment.user = current_user
+
+    if comment.save
+      render json: comment, status: :ok
+    else
+      render json: { error: "コメントができませんでした。"}, status: :unprocessable_entity
+    end
   end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content)
+    end
 end
