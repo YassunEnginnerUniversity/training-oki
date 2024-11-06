f2252d76007a34251a8dce55c8bbedee4cec1004
kei-1313 <candydog666@gmail.com>
2024-11-06 21:53:50 +0800

    add: モデルの作成とpost, userとの関連付けを追加

--- api-and-rspec-training/app/models/post.rb
+++ api-and-rspec-training/app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :likes, dependent: :destroy
+  has_many :liked_users, through: :likes, source: :user
+
   validates :user_id, presence: true
   validates :content, presence: true
+
 end
