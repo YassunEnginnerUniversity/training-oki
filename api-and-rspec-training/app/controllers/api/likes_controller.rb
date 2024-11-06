59a5772a533c4fc59dd516facc48f59379f743c7
kei-1313 <candydog666@gmail.com>
2024-11-06 22:58:24 +0800

    add: likesのテストを追加

--- api-and-rspec-training/app/controllers/api/likes_controller.rb
+++ api-and-rspec-training/app/controllers/api/likes_controller.rb
@@ -2,13 +2,14 @@ class Api::LikesController < ApplicationController
   before_action :authenticate_user! # セッションを保持しているかアクションの前に確認
 
   def create
-    post = Post.find(params[:id])
-    like = post.likes.build(user:current_user)
+    post = Post.find(params[:post_id])
+    like = post.likes.build(user: current_user)
+  
 
     if like.save
-      render json: { message: "いいねしました。"}, status: :ok
+      render json: { message: "いいねしました。" }, status: :ok
     else
-      render json: { error: "すでにいいね済みです。"}, status: :unprocessable_entity
+      render json: { error: "すでにいいね済みです。" }, status: :unprocessable_entity
     end
 
     rescue ActiveRecord::RecordNotFound
--- api-and-rspec-training/spec/requests/api/likes_spec.rb
+++ api-and-rspec-training/spec/requests/api/likes_spec.rb
@@ -1,5 +1,44 @@
 require 'rails_helper'
 
 RSpec.describe "Api::Likes", type: :request do
-  
+  let!(:user) { FactoryBot.create(:user)}
+  let!(:other_user_id) { 9999 }
+  let!(:other_user_post_id) { 9999 }
+
+  describe "POST /api/posts/{post_id}/like" do
+    context "セッションで認証されている場合" do
+      before do
+        post "/api/login", params: { username: user.username, password: user.password }  # 事前にログインをしておく
+      end
+
+      it "投稿に対していいねができる" do
+        user_post = FactoryBot.create(:post, user: user)
+        post "/api/posts/#{user_post.id}/like"
+
+        expect(response).to have_http_status(:ok)
+        json_response = JSON.parse(response.body)
+        expect(json_response["message"]).to eq("いいねしました。")
+      end
+
+      it "重複していいねした場合、ステータスは422でエラーメッセージを返す" do
+        user_post = FactoryBot.create(:post, user: user)
+        post "/api/posts/#{user_post.id}/like"  # 最初の「いいね」
+        post "/api/posts/#{user_post.id}/like"  # 2回目の「いいね」
+    
+        expect(response).to have_http_status(:unprocessable_entity)
+        json_response = JSON.parse(response.body)
+        expect(json_response["error"]).to eq("すでにいいね済みです。")
+      end
+    end
+
+    context "セッションで認証されていない場合" do
+      let!(:user_post) { FactoryBot.create(:post, user: user)}
+      it "ステータスは401で認証エラーメッセージを返す" do
+        post "/api/posts/#{user_post.id}/like"
+    
+        expect(response).to have_http_status(:unauthorized)
+        expect(JSON.parse(response.body)).to eq("error" => "認証されていないアクセスです。")
+      end
+    end
+  end
 end
