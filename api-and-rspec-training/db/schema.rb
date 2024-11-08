e8a54e5c97a3e395184d18efd8b0bb741e447259
kei-1313 <candydog666@gmail.com>
2024-11-09 00:25:08 +0800

    add: relationshipモデルとコントローラーを作成

--- api-and-rspec-training/db/schema.rb
+++ api-and-rspec-training/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_06_161749) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_08_162220) do
   create_table "comments", force: :cascade do |t|
     t.text "content", null: false
     t.integer "user_id", null: false
@@ -38,6 +38,13 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_06_161749) do
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
