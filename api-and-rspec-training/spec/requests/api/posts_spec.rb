fe137180428c7a55cebede1305520ebcb8e11cef
kei-1313 <candydog666@gmail.com>
2024-11-12 22:49:22 +0800

    clean : postsのshowアクションをshow_specに切り分け、リファクタリング

--- api-and-rspec-training/spec/requests/api/posts_spec.rb
+++ api-and-rspec-training/spec/requests/api/posts_spec.rb
@@ -5,47 +5,6 @@ RSpec.describe "Api::Posts", type: :request do
   let!(:other_user_id) { 9999 }
   let!(:other_user_post_id) { 9999 }
 
-  describe "GET /api/posts/{id}" do
-    let!(:user_post) { FactoryBot.create(:post, user: user) }
-    context "セッションで認証されている場合" do
-      before do
-        post "/api/login", params: { username: user.username, password: user.password }
-      end
-
-      it "投稿が存在する場合、ステータスは200で投稿の情報を返す" do
-        get "/api/posts/#{user_post.id}"
-        expect(response).to have_http_status(:ok)
-
-        json_response = JSON.parse(response.body)
-        expect(json_response["id"]).to eq(user_post.id)
-        expect(json_response["content"]).to eq(user_post.content)
-        expect(json_response["user"]["id"]).to eq(user_post.user.id)
-        expect(json_response["user"]["username"]).to eq(user_post.user.username)
-        expect(json_response["likes_count"]).to eq(user_post.likes.count)
-        user_post.comments.each_with_index do |comment, index|
-          expect(json_response["comments"][index]["id"]).to eq(comment.id)
-          expect(json_response["comments"][index]["content"]).to eq(comment.content)
-          expect(json_response["comments"][index]["user"]["id"]).to eq(comment.user.id)
-          expect(json_response["comments"][index]["user"]["username"]).to eq(comment.user.username)
-        end
-      end
-
-      it "存在しない投稿のIDをリクエストすると404を返す" do
-        get "/api/posts/#{other_user_post_id}"
-        expect(response).to have_http_status(:not_found)
-        expect(JSON.parse(response.body)).to eq("error" => "該当する投稿が見つかりませんでした。")
-      end
-    end
-
-    context "セッションで認証されていない場合" do
-      it "ステータスは401で認証エラーメッセージを返す" do
-        get "/api/posts/#{user_post.id}"
-        expect(response).to have_http_status(:unauthorized)
-        expect(JSON.parse(response.body)).to eq("error" => "認証されていないアクセスです。")
-      end
-    end
-  end
-
   describe "POST /api/posts/" do
     let!(:user_post) { FactoryBot.create(:post, user: user) }
     context "セッションで認証されている場合" do
