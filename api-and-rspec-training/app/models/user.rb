51b6cd1264647b3a5881d990f5c625816f03b0ca
kei-1313 <candydog666@gmail.com>
2024-11-13 23:31:46 +0800

    add: annotateを実行して、annotationをつける

--- api-and-rspec-training/app/models/user.rb
+++ api-and-rspec-training/app/models/user.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  password_digest :string
+#  username        :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+# Indexes
+#
+#  index_users_on_username  (username) UNIQUE
+#
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :likes, dependent: :destroy
--- api-and-rspec-training/spec/factories/follow_users.rb
+++ api-and-rspec-training/spec/factories/follow_users.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: follow_users
+#
+#  id          :integer          not null, primary key
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  followed_id :integer
+#  follower_id :integer
+#
+# Indexes
+#
+#  index_follow_users_on_follower_id_and_followed_id  (follower_id,followed_id) UNIQUE
+#
 FactoryBot.define do
   factory :follow_user do
     follower_id { 1 }
