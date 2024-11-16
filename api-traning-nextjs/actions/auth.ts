f4d661a18a7aea5ec9a44ed468e97f70dc5ebda7
kei-1313 <candydog666@gmail.com>
2024-11-16 16:32:12 +0800

    add: credentialsを追加
    /api/loginにPOSTリクエストを送る際にcookieの情報を含めるようにするため

--- api-traning-nextjs/actions/auth.ts
+++ api-traning-nextjs/actions/auth.ts
@@ -2,18 +2,22 @@
 import { LoginResponse } from "@/types/api/response/types";
 import { redirect } from "next/navigation";
 
+//メモ zodでフロントエンド側のバリデーションをつける（余計なPOSTを送らせないようにする）
+
 export const login = async (prevState: LoginResponse, formData: FormData): Promise<LoginResponse> => {
   const username = formData.get("username");
   const password = formData.get("password");
-
   const data = { username, password }
   const endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT + "/api/login"
 
   // /api/login にPOSTでデータを送る
   const response = await fetch(endpoint, {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    credentials: 'include',
     body: JSON.stringify(data),
-    headers: { "Content-Type": "application/json" }
   })
 
   // ログインに失敗し、エラーが発生したとき
